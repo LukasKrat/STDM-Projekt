ae828ca8f8fcad02e1f8e078248647e0346ff307
LukasKrat <vy388@gmx.de>
2024-01-31 09:40:03 +0100

    implemented more classes;

--- Vereinsverwaltung/src/vereinsverwaltung/Mitglied.java
+++ Vereinsverwaltung/src/vereinsverwaltung/Mitglied.java
@@ -9,5 +9,56 @@ package vereinsverwaltung;
  * @author lukas
  */
 public class Mitglied {
+    private String name;
+    private String telefon;
+    private String email;
+    private VereinsAbteilung abteilung;
+    private String adresse;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public VereinsAbteilung getAbteilung() {
+        return abteilung;
+    }
+
+    public void setAbteilung(VereinsAbteilung abteilung) {
+        this.abteilung = abteilung;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
     
+    public Mitglied(String name, String telefon, String email, String adresse) {
+        this.name = name;
+        this.telefon = telefon;
+        this.email = email;
+        this.adresse = adresse;
+    }
 }
