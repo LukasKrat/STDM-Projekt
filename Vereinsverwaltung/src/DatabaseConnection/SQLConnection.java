ad89915383805cf2b2fd1b251a82137e41bfa25d
Nikita <nikistosov@gmail.com>
2024-02-07 09:09:08 +0100

    feat: DB connection
    - Connect project with DB
    - Later need to add a URL for connection

--- Vereinsverwaltung/src/DatabaseConnection/SQLConnection.java
+++ Vereinsverwaltung/src/DatabaseConnection/SQLConnection.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DatabaseConnection;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Nikita
  */
 public class SQLConnection {
-    
+        public static Connection connect(String dbPath) {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:" + dbPath;
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite successful");
+        } catch (SQLException e) {
+            System.err.println("Connection error: " + e.getMessage());
+        }
+        return connection;
+    }
 }
