4f246783a72c727205036af610ea72e02ac16ab3
Nikita <nikistosov@gmail.com>
2024-02-07 09:49:53 +0100

    feat: SQL Mitglied connection
    SQL Mitglied connection

---
+++ Vereinsverwaltung/src/DatabaseConnection/SQLMitglied.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ *
+ * @author Nikita
+ */
+public class SQLMitglied {
+    public static void main(String[] args) {
+        String dbPath = "/111.db"; //must write the path 
+        Connection connection = SQLiteConnection.connect(dbPath);
+
+        String query = "SELECT * FROM Mitglied;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection error: " + e.getMessage());
+        }
+    }
+}
+
